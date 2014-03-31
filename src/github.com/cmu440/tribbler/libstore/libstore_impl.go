219fb91af2a2e2e740a2b7bbcdece2c1e9f70aab
Salem Hilal <salemhilal@gmail.com>
2014-03-31 16:05:41 -0400

    libstore, minus leases. Compiles, needs tested.

--- src/github.com/cmu440/tribbler/libstore/libstore_impl.go
+++ src/github.com/cmu440/tribbler/libstore/libstore_impl.go
@@ -2,13 +2,30 @@ package libstore
 
 import (
 	"errors"
+	"net/rpc"
+	"time"
 
+	"github.com/cmu440/tribbler/rpc/librpc"
 	"github.com/cmu440/tribbler/rpc/storagerpc"
 )
 
 type libstore struct {
-	// TODO: implement this!
+	// List of storage servers
+	servers []storagerpc.Node
+
+	// host:port of the master storage server
+	masterServerHostPort string
+
+	// host:port of this instance of the libstore
+	hostPort string
+
+	// Maps storage nodeID's to their respective clients.
+	serverClients map[uint32]*rpc.Client
 }
+
+const (
+	RETRY_LIMIT = 5
+)
 
 // NewLibstore creates a new instance of a TribServer's libstore. masterServerHostPort
 // is the master storage server's host:port. myHostPort is this Libstore's host:port
@@ -35,29 +52,255 @@ type libstore struct {
 // need to create a brand new HTTP handler to serve the requests (the Libstore may
 // simply reuse the TribServer's HTTP handler since the two run in the same process).
 func NewLibstore(masterServerHostPort, myHostPort string, mode LeaseMode) (Libstore, error) {
-	return nil, errors.New("not implemented")
+	// Get to leases later.
+	if mode != Never {
+		return nil, errors.New("Mode not implemented")
+	}
+
+	// Connect to server
+	masterServerClient, err := dialRpcHostport(masterServerHostPort)
+	if err != nil {
+		_DEBUGLOG.Println("Failed to dial storage server", err)
+	}
+
+	// Get server list. If not ready, sleep for a second, retry 5 times
+	args := &storagerpc.GetServersArgs{}
+	var reply storagerpc.GetServersReply
+	err = masterServerClient.Call("GetServers", args, &reply)
+
+	// Should we try and timeout (i.e. is err == nil)?
+	for attempts := 1; err == nil && reply.Status == storagerpc.NotReady; attempts++ {
+		// Have we timed out?
+		if attempts >= RETRY_LIMIT {
+			return nil, errors.New("Connection timeout calling GetServers")
+		}
+		time.Sleep(1 * time.Second)                              // Sleep a second
+		err = masterServerClient.Call("GetServers", args, reply) // Try to call again
+	}
+	// At this point, we should have a list of servers.
+	// Pack up the libstore
+	lib := &libstore{
+		servers:              reply.Servers,
+		masterServerHostPort: masterServerHostPort,
+		hostPort:             myHostPort,
+		serverClients:        make(map[uint32]*rpc.Client),
+	}
+
+	// Register the lease callback rpc
+	rpc.RegisterName("LeaseCallbacks", librpc.Wrap(lib))
+
+	// Send 'er off.
+	return lib, nil
 }
 
+//
+// API IMPLEMENTATION
+//
+
+// Gets a key's singleton value from the data store, cacheing if necessary
 func (ls *libstore) Get(key string) (string, error) {
-	return "", errors.New("not implemented")
+	// Get the server the key belongs on.
+	client, err := ls.getClientForKey(key)
+	if err != nil {
+		return "", err
+	}
+
+	// response and args
+	var reply storagerpc.GetReply // Create reply
+	args := &storagerpc.GetArgs{
+		Key:       key,               // Specificy key
+		WantLease: ls.wantLease(key), // Do we want to cache this key?
+		HostPort:  ls.hostPort,       // Our hostport
+	}
+
+	// make the call
+	if err := client.Call("Get", args, &reply); err != nil {
+		return "", err
+	}
+
+	// Handle the response
+	switch reply.Status {
+	case storagerpc.WrongServer: // Wrong server
+		_ERRORLOG.Println("tried to get data from wrong server. key: ", key)
+		return "", errors.New("tried to get data from wrong server")
+	case storagerpc.KeyNotFound: // Can't find the key
+		return "", errors.New("key not found")
+	case storagerpc.OK: // All is well
+		return reply.Value, nil
+	default: // Not sure what happened
+		_ERRORLOG.Println("unhandled status: ", reply.Status)
+		return "", errors.New("unhandled status")
+	}
+
 }
 
 func (ls *libstore) Put(key, value string) error {
-	return errors.New("not implemented")
+	// Get the server the key belongs on.
+	client, err := ls.getClientForKey(key)
+	if err != nil {
+		return err
+	}
+
+	// response and args
+	var reply storagerpc.PutReply
+	args := &storagerpc.PutArgs{
+		Key:   key,
+		Value: value,
+	}
+
+	// Make the call
+	if err := client.Call("Put", args, &reply); err != nil {
+		return err
+	}
+
+	// Handle the response
+	switch reply.Status {
+	case storagerpc.WrongServer: // Wrong server
+		_ERRORLOG.Println("tried to get data from wrong server. key: ", key)
+		return errors.New("tried to get data from wrong server")
+	case storagerpc.OK: // All is well
+		return nil
+	default: // Not sure what happened
+		_ERRORLOG.Println("unhandled status: ", reply.Status)
+		return errors.New("unhandled status")
+	}
 }
 
 func (ls *libstore) GetList(key string) ([]string, error) {
-	return nil, errors.New("not implemented")
+	// Get the server the key belongs on.
+	client, err := ls.getClientForKey(key)
+	if err != nil {
+		return nil, err
+	}
+
+	// response and args
+	var reply storagerpc.GetListReply
+	args := &storagerpc.GetArgs{
+		Key:       key,
+		WantLease: ls.wantLease(key),
+		HostPort:  ls.hostPort,
+	}
+
+	// make the call
+	if err := client.Call("GetList", args, &reply); err != nil {
+		return nil, err
+	}
+
+	// handle the response
+	switch reply.Status {
+	case storagerpc.WrongServer:
+		_ERRORLOG.Println("tried to get data from wrong server. key: ", key)
+		return nil, errors.New("tried to get data from wrong server")
+	case storagerpc.KeyNotFound:
+		return nil, errors.New("key not found")
+	case storagerpc.OK:
+		return reply.Value, nil
+	default:
+		_ERRORLOG.Println("unhandled status: ", reply.Status)
+		return nil, errors.New("unhandled status")
+	}
 }
 
 func (ls *libstore) RemoveFromList(key, removeItem string) error {
-	return errors.New("not implemented")
+	// Get the server the key belongs on.
+	client, err := ls.getClientForKey(key)
+	if err != nil {
+		return err
+	}
+
+	// response and args
+	var reply storagerpc.PutReply
+	args := &storagerpc.PutArgs{
+		Key:   key,
+		Value: removeItem,
+	}
+
+	// Make the call
+	if err := client.Call("RemoveFromList", args, &reply); err != nil {
+		return err
+	}
+
+	// handle the response
+	switch reply.Status {
+	case storagerpc.WrongServer:
+		_ERRORLOG.Println("tried to remove data from wrong server. key: ", key)
+		return errors.New("tried to remove data from wrong server")
+	case storagerpc.ItemNotFound:
+		return errors.New("key not found")
+	case storagerpc.OK:
+		return nil
+	default:
+		_ERRORLOG.Println("unhandled status: ", reply.Status)
+		return errors.New("unhandled status")
+	}
 }
 
 func (ls *libstore) AppendToList(key, newItem string) error {
-	return errors.New("not implemented")
+	// Get the server the key belongs on
+	client, err := ls.getClientForKey(key)
+	if err != nil {
+		return err
+	}
+
+	// response and args
+	var reply storagerpc.PutReply
+	args := storagerpc.PutArgs{
+		Key:   key,
+		Value: newItem,
+	}
+
+	// Make the call
+	if err := client.Call("AppendToList", args, &reply); err != nil {
+		return err
+	}
+
+	switch reply.Status {
+	case storagerpc.WrongServer:
+		_ERRORLOG.Println("tried to append data from wrong server. key: ", key)
+		return errors.New("tried to appen data from wrong server")
+	case storagerpc.ItemExists:
+		return errors.New("item exists")
+	case storagerpc.OK:
+		return nil
+	default:
+		_ERRORLOG.Println("unhandled status: ", reply.Status)
+		return errors.New("unhandled status")
+	}
 }
 
 func (ls *libstore) RevokeLease(args *storagerpc.RevokeLeaseArgs, reply *storagerpc.RevokeLeaseReply) error {
 	return errors.New("not implemented")
+}
+
+//
+// NON-API / HELPER METHODS
+//
+
+// Given a node, get its client. Create it if it isn't already created.
+// Should be called synchronously.
+func (ls *libstore) getClientForNode(node *storagerpc.Node) (*rpc.Client, error) {
+
+	if client, ok := ls.serverClients[node.NodeID]; ok == true {
+		// Already have a clilent, return it
+		return client, nil
+	} else if client, err := dialRpcHostport(node.HostPort); err == nil {
+		// Lazily created new client, save it for later.
+		ls.serverClients[node.NodeID] = client
+		return client, nil
+	} else {
+		// Something threw an error, spit that out.
+		return nil, err
+	}
+}
+
+// Given a key, spit out a client to the appropriate node
+func (ls *libstore) getClientForKey(key string) (*rpc.Client, error) {
+	node := getNodeForHashKey(ls.servers, key)
+	return ls.getClientForNode(node)
+}
+
+// Determine whether or not we want a lease on a specified key
+func (ls *libstore) wantLease(key string) bool {
+	// TODO: Implement this
+	return false
 }
---
+++ src/github.com/cmu440/tribbler/libstore/util_libstore.go
@@ -0,0 +1,61 @@
+package libstore
+
+import (
+	"log"
+	"net/rpc"
+	"os"
+	"sort"
+	"strings"
+
+	"github.com/cmu440/tribbler/rpc/storagerpc"
+)
+
+var _DEBUGLOG = log.New(os.Stdout, "LBS-DEBUG: ", log.Lmicroseconds|log.Lshortfile)
+var _ERRORLOG = log.New(os.Stdout, "LBS-ERROR: ", log.Lmicroseconds|log.Lshortfile)
+
+// Convenience wrapper for DialHTTP
+func dialRpcHostport(hostport string) (*rpc.Client, error) {
+	return rpc.DialHTTP("tcp", hostport)
+}
+
+// finds the index of the given nodeID in the given hash ring
+// returns -1 if the given nodeID is not one of the hash ring's points
+func getHashRingNodeIndex(hashRing []storagerpc.Node, nodeID uint32) int {
+	searchFn := func(i int) bool { return hashRing[i].NodeID >= nodeID }
+
+	// use builtin binary search to search for index
+	foundIndex := sort.Search(len(hashRing), searchFn)
+	// if we found the node
+	if foundIndex < len(hashRing) && hashRing[foundIndex].NodeID == nodeID {
+		return foundIndex
+	}
+	return -1
+}
+
+// Gets the hash value of the key, based on the substring before the first colon
+// in the key, if available
+func HashKeyPrefix(key string) uint32 {
+	sep := ":"
+	numSubstrs := 2
+
+	prefix := strings.SplitN(key, sep, numSubstrs)[0]
+	return StoreHash(prefix)
+}
+
+// Given a hash ring and a key, returns the Node that the key hashes to.
+// Assumes hashRing sorted in increasing order by NodeID
+func getNodeForHashKey(hashRing []storagerpc.Node, key string) *storagerpc.Node {
+	// Get hash of key prefix
+	hash := HashKeyPrefix(key)
+
+	// Go over each hash element
+	for _, node := range hashRing {
+		if node.NodeID > hash {
+			return &node
+		}
+	}
+
+	// This is mostly for sanity checking, we should never hit this segment.
+	_DEBUGLOG.Fatalln("No server in ring found for hash: ", key)
+	return nil
+}
